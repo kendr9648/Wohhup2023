e72244e0b239885acb4fc2d5efe09a9a24484c41
william Kendrick <williamk9648@gmail.com>
2023-07-03 16:34:58 +0800

    First commit

---
+++ Wohhup2023/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  Wohhup2023
+//
+//  Created by William Kendrick on 3/7/23.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+}
---
+++ Wohhup2023/ViewController2.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController2.swift
+//  Wohhup2023
+//
+//  Created by William Kendrick on 3/7/23.
+//
+
+import SwiftUI
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    private let imageView: UIImageView =  {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "Wohhuplogo")
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5 , execute: {
+            self.animate()
+        } )
+    }
+    
+    private func animate() {
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 3
+            let diffx = size - self.view.frame.size.width
+            let diffy = self.view.frame.size.height - size
+            
+            
+            self.imageView.frame = CGRect(x: -(diffx/2), y: diffy/2, width: size, height: size)
+        })
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+            
+        }, completion: { done in
+            if done {
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute:{
+                    performSegue(withIdentifier: "segue1", sender: self)
+                      
+            })
+            }
+        })
+        
+        
+        
+    }
+    
+
+}
