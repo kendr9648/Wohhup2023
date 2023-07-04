ca962b8ac63d4856ecb87b118905e8151a8cec19
william Kendrick <williamk9648@gmail.com>
2023-07-04 10:01:30 +0800

    Added subviews

--- Wohhup2023/ProjectsViewController.swift
+++ Wohhup2023/ProjectsViewController.swift
@@ -13,9 +13,22 @@ class ProjectsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        PastView.isHidden = true
+        OngoingView.isHidden = true
+        
+    }
+    @IBOutlet weak var OngoingView: UIView!
+    @IBOutlet weak var PastView: UIView!
+    
+    @IBAction func OngoingBtnPress(_ sender: Any) {
+        OngoingView.isHidden = false
+        PastView.isHidden = true
     }
     
-    
+    @IBAction func PastBtnPress(_ sender: Any) {
+        PastView.isHidden = false
+        OngoingView.isHidden = true
+    }
     
 }
 
